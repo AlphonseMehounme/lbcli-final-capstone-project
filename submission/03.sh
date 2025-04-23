3032e1b2dad5afb5fb946317943569d4449280b6
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-04-23 20:17:23 +0100

    transaction

--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,6 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+COINBASE_TXID=$(bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216128) 2 | jq -r '.tx[0].txid')
+COINBASE_VOUT=0
+TRANSACTION=$(bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216351) 2 | jq --arg COINBASE_TXID "$COINBASE_TXID" '.tx[] | select(.vin[].txid == $COINBASE_TXID)')
+TRANSACTION_ID=$(echo $TRANSACTION | jq -r '.txid')
+echo $TRANSACTION_ID
